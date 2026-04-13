7fad9579dfe762dad0d51c5a30297683e1f88330
mat6784 <mathak@LAPTOP-EL9MJ4KF.localdomain>
2026-04-13 15:50:23 +0200

    voilà Louis c'est fait !

--- simulateur/BouttonEntree.java
+++ simulateur/BouttonEntree.java
@@ -1,5 +1,4 @@
 package simulateur;
-import java.util.*;
 
 public class BouttonEntree {
 
@@ -11,24 +10,8 @@ public class BouttonEntree {
 		this.numero = numero;
 	}
 
-	public String action() {
-		switch (S.getValeur(numero)) {
-			case Etat.ND:
-				S.setValeur(numero,Etat.DW);
-				return Etat.DW.toString();
-			case Etat.DW:
-				S.setValeur(numero,Etat.UP);
-				return Etat.UP.toString();
-			case Etat.UP:
-				S.setValeur(numero,Etat.DW);
-				return Etat.DW.toString();
-			case default:
-				return "?";
-		}
+	public String set(Etat E) {
+		S.setValeur(numero,E);
 	}
 	
-	public String getValeur() {
-		return S.getValeur(numero).toString();
-	}
-
 }
