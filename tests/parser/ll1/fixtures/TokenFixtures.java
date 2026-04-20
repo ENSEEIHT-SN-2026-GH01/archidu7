8e1c6d1173944b3d9c1db1645567f1d0915c5bfd
Alexis Briend <alexis.briend375@gmail.com>
2026-04-20 19:20:10 +0200

    test(fixtures): TokenFixtures accepte offset (defaut 0 pour retrocompat)

--- tests/parser/ll1/fixtures/TokenFixtures.java
+++ tests/parser/ll1/fixtures/TokenFixtures.java
@@ -1,15 +1,11 @@
 package tests.parser.ll1.fixtures;
 
-import parser.ll1.token.*;
-import java.util.*;
+import parser.ll1.token.Token;
+import parser.ll1.token.TokenType;
 
 public final class TokenFixtures {
-    public static Token tok(TokenType t) { return new Token(t, null, 1, 1); }
-    public static Token tok(TokenType t, String v) { return new Token(t, v, 1, 1); }
-    public static Token tok(TokenType t, String v, int line, int col) { return new Token(t, v, line, col); }
-    public static List<Token> seq(Token... ts) {
-        List<Token> l = new ArrayList<>(Arrays.asList(ts));
-        l.add(tok(TokenType.EOF));
-        return l;
-    }
+    public static Token tok(TokenType t) { return new Token(t, null, 1, 1, 0); }
+    public static Token tok(TokenType t, String v) { return new Token(t, v, 1, 1, 0); }
+    public static Token tok(TokenType t, String v, int line, int col) { return new Token(t, v, line, col, 0); }
+    public static Token tok(TokenType t, String v, int line, int col, int offset) { return new Token(t, v, line, col, offset); }
 }
