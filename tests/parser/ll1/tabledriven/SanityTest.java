88b66b48905d9eae43035388c91b0da44a82689c
Alexis Briend <alexis.briend375@gmail.com>
2026-04-30 18:26:32 +0200

    chore(setup): arborescence tabledriven + sanity

---
+++ tests/parser/ll1/tabledriven/SanityTest.java
@@ -0,0 +1,8 @@
+package tests.parser.ll1.tabledriven;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SanityTest {
+    @Test public void vrai() { assertTrue(true); }
+}
