6d17ac897ba7fc08002eef351e2b1aeb07af6052
evva49 <ewavaillant@zohomail.eu>
2026-05-23 09:57:26 +0200

    maj barre boutons

--- src/FenetrePrincipale.java
+++ src/FenetrePrincipale.java
@@ -24,17 +24,23 @@ public class FenetrePrincipale extends Scene {
         FileStorage stockage = new TextFileStorage(); 
 
         BoutonsPrincipale boutons = new BoutonsPrincipale(editeur, stockage);
-        boutons.setStyle("-fx-background-color: white; -fx-padding: 8px 15px;"); //on espace un peu les bouton
+        boutons.setStyle("-fx-background-color: transparent;"); //on espace un peu les bouton
 
         ListeModulePrincipale environnement = new ListeModulePrincipale(editeur, stockage);
         // Style pour l'environnement
         environnement.setStyle("-fx-background-color: #f8fafc; -fx-border-color: #e2e8f0; -fx-border-width: 0 1px 0 0;");
         
         MenuPrincipale menu = new MenuPrincipale();
-        menu.setStyle("-fx-background-color: #f1f5f9; -fx-border-color: #e2e8f0; -fx-border-width: 0 0 1px 0; -fx-padding: 2px 5px;");
+        menu.setStyle("-fx-background-color: transparent; -fx-font-size: 14px;");
 
-        VBox outils = new VBox(menu, boutons);
-        outils.setStyle("-fx-background-color: white; -fx-border-color: #cbd5e1; -fx-border-width: 0 0 1px 0;"); //délimitation de l'en tête par rapport à la zone de code
+        BorderPane outils = new BorderPane();
+        outils.setLeft(menu);
+        outils.setRight(boutons);
+
+        BorderPane.setAlignment(menu, javafx.geometry.Pos.CENTER_LEFT);
+        BorderPane.setAlignment(boutons, javafx.geometry.Pos.CENTER_RIGHT);
+
+        outils.setStyle("-fx-background-color: #f8fafc; -fx-border-color: #e2e8f0; -fx-border-width: 0 0 1px 0; -fx-padding: 4px 10px;"); //délimitation de l'en tête par rapport à la zone de code
  
         root.setCenter(editeur);
         root.setTop(outils);
